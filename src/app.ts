baf5e89312b4b5846860764fe0aeb36a00510617
J2mF <mp4-12cs5@hotmail.com>
2022-10-27 16:11:14 +0300

    Add form style and submit handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     private templateElement: HTMLTemplateElement;
     private hostElement: HTMLDivElement;
     private element: HTMLFormElement;
+    private titleInputElement: HTMLInputElement;
+    private descriptionInputElement: HTMLInputElement;
+    private peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,11 +12,27 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector("#title")! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
     private attach() {
         this.hostElement.insertAdjacentHTML('afterbegin', this.element);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+
     }
 }
 
