917ede2e9685d586c15d0ea890549f09fbba16c0
J2mF <mp4-12cs5@hotmail.com>
2022-11-07 21:11:59 +0300

    refactor binding to decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+// autobind decorator
+function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        }
+    };
+
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     private templateElement: HTMLTemplateElement;
     private hostElement: HTMLDivElement;
@@ -27,12 +45,12 @@ class ProjectInput {
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-
     }
 }
 
