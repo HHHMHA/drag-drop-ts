6cb5acc87b84f51c6b88f237a59648bf65111456
J2mF <mp4-12cs5@hotmail.com>
2022-12-19 17:25:53 +0300

    fix: duplicate projects behaviour

--- src/app.ts
+++ src/app.ts
@@ -146,7 +146,12 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            this.assignedProjects = projects.filter(element => {
+                if (this.type == 'active') {
+                    return element.status === ProjectStatus.ACTIVE;
+                }
+                return element.status === ProjectStatus.FINISHED;
+            });
             this.renderProjects()
         });
 
@@ -165,6 +170,7 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(this.listId)! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const projectItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = projectItem.title;
